b8d44868eed9d80703ed2c038faadc4e21304416
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-04-04 02:44:59 +0530

    Add files via upload

---
+++ average_salary.py
@@ -0,0 +1,14 @@
+#1491. Average Salary Excluding the Minimum and Maximum Salary
+
+
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        min_salary = min(salary)
+        max_salary = max(salary)
+        sum_salary = sum(salary) - min_salary - max_salary
+        count = len(salary) - 2
+
+        average_salary = sum_salary / count
+
+        return average_salary
+        
