1cd82e0bea69b5780c991f9211bee1dd8fb534be
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-04-17 23:17:32 +0530

    Update count_odd_between.py

--- count_odd_between.py
+++ count_odd_between.py
@@ -1,7 +1,6 @@
 '''Given two non-negative integers low and high.
    Return the count of odd numbers between low and high (inclusive).'''
 
-
 low = int(input("Enter the lower bound: "))
 high = int(input("Enter the upper bound: "))
 
